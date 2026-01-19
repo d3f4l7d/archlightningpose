7a140da3e5d335d3ad0a6d5b6a1615d9864aa530
d3f4l7d <idaten.bekko@gmail.com>
2026-01-19 19:04:43 +0900

    Update devinstall.sh

--- devinstall.sh
+++ devinstall.sh
@@ -1,21 +1,15 @@
 echo
-echo " ____  _______     __"
-echo "|  _ \| ____\ \   / /"
-echo "| | | |  _|  \ \ / / "
-echo "| |_| | |___  \ V /  "
-echo "|____/|_____|  \_/   "
+echo " _     ___ ____ _   _ _____ _   _ ___ _   _  ____   ____   ___  ____  _____ "
+echo "| |   |_ _/ ___| | | |_   _| \ | |_ _| \ | |/ ___| |  _ \ / _ \/ ___|| ____|"
+echo "| |    | | |  _| |_| | | | |  \| || ||  \| | |  _  | |_) | | | \___ \|  _|  "
+echo "| |___ | | |_| |  _  | | | | |\  || || |\  | |_| | |  __/| |_| |___) | |___ "
+echo "|_____|___\____|_| |_| |_| |_| \_|___|_| \_|\____| |_|    \___/|____/|_____|"
 echo
 
-sudo pacman -Syu --noconfirm ffmpeg nvtop s-tui
+sudo pacman -Syu --noconfirm ffmpeg
 
-git clone https://github.com/paninski-lab/lightning-pose.git ~/.config/lightning-pose
-git clone https://github.com/paninski-lab/lightning-pose-app.git ~/.config/lightning-pose-app
-cd ~/.config/lightning-pose
-rm -rf .git .github
-~/.pyenv/versions/venv10/bin/pip install -e ".[dev.extra_models]"
-cd ~/.config/lightning-pose-app
-rm -rf .git .github
-~/.pyenv/versions/venv10/bin/pip install -e ".[dev.extra_models]"
-~/.pyenv/versions/venv10/bin/litpose --help
+~/.pyenv/versions/venv10/bin/pip install lightning-pose
+~/.pyenv/versions/venv10/bin/pip install lightning-pose-app
+~/.pyenv/versions/venv10/bin/litpose run_app
 
 echo "autoinstall of lightning-pose -- DONE"
